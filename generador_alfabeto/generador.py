9f0bdf7c4d3df15da50d50bdfb84b0046d1bf816
Gabilorium <chgabiv@gmail.com>
2026-07-11 18:24:04 -0300

    feat: build initial letter-to-code dictionary

--- generador_alfabeto/generador.py
+++ generador_alfabeto/generador.py
@@ -1,15 +1,25 @@
 import random
 import string
 
+## CONSTANTES:
 SIMBOLOS = ['$', '<', '-', '>', '&']
 SEED = 42
 CANTIDAD_SIMBOLOS_LETRA = 3
+CANT_CARACTERES_A_CONVERTIR = len(string.ascii_lowercase)
+
 random.seed(SEED)
+diccionario = {}
 
 
-print('Letras de %s simbolos'%(CANTIDAD_SIMBOLOS_LETRA))
-letra_nueva = ""
-for i in range(CANTIDAD_SIMBOLOS_LETRA):
-    letra_nueva = letra_nueva + random.choice(SIMBOLOS)
+for i in range (CANT_CARACTERES_A_CONVERTIR):
+    letra_a_codificar = string.ascii_lowercase[i]
+    letra_codificada = ""
+    print("Letra = %a" %(letra_a_codificar))
+    for j in range(CANTIDAD_SIMBOLOS_LETRA):
+        letra_codificada = letra_codificada + random.choice(SIMBOLOS)
+        diccionario[letra_a_codificar] = letra_codificada
+        
 
-print("Letra = %a" %(letra_nueva))
+
+for clave, valor in diccionario.items():
+    print(clave, valor)
