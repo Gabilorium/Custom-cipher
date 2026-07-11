2d4ae95d765d29b54309c7dcd5f5e1b20e0ae809
Gabilorium <chgabiv@gmail.com>
2026-07-11 20:58:07 -0300

    fix: generate unique lowercase letter codes via rejection sampling (closes #1)

--- alphabet_generator/generator.py
+++ alphabet_generator/generator.py
@@ -1,25 +1,36 @@
 import random
 import string
 
-## CONSTANTES:
+## CONSTS:
 ELIGIBLE_SYMBOLS = ['$', '<', '-', '>', '&']
 SEED = 42
 SYMBOLS_PER_LETTER = 3
 CHARACTERS_TO_CONVERT = len(string.ascii_lowercase)
 
+
 random.seed(SEED)
 dicionary = {}
+code_set = set()
 
+##FUNCTIONS
+def encode_letter ():
+    while True:
+        encoded_letter = ""   # reset en cada intento
+        for j in range(SYMBOLS_PER_LETTER):
+            encoded_letter = encoded_letter + random.choice(ELIGIBLE_SYMBOLS)
+        if encoded_letter not in code_set:   # ¿qué condición te dice "este código sirve, salí del loop"?
+            break
+    return encoded_letter
 
+## """MAIN"""
 for i in range (CHARACTERS_TO_CONVERT):
     letter_to_encode = string.ascii_lowercase[i]
-    encoded_letter = ""
-    print("Letter = %a" %(letter_to_encode))
-    for j in range(SYMBOLS_PER_LETTER):
-        encoded_letter = encoded_letter + random.choice(ELIGIBLE_SYMBOLS)
-        dicionary[letter_to_encode] = encoded_letter
+    encoded_letter = encode_letter()
+    dicionary[letter_to_encode] = encoded_letter
+    code_set.add(encoded_letter)
         
-
+for code in code_set:
+    print (code)
 
 for key, value in dicionary.items():
     print(key, value)
