63e151a0379e756d2a56e6e2301970e2cf044137
Gabilorium <chgabiv@gmail.com>
2026-07-13 13:51:49 -0300

    feat: add uppercase, digits and punctuation characters

--- alphabet_generator/generator.py
+++ alphabet_generator/generator.py
@@ -5,32 +5,42 @@ import string
 ELIGIBLE_SYMBOLS = ['$', '<', '-', '>', '&']
 SEED = 42
 SYMBOLS_PER_LETTER = 3
-CHARACTERS_TO_CONVERT = len(string.ascii_lowercase)
+##LOWERCASE_CHARS = len(string.ascii_lowercase)
+##UPPERCASE_CHARS = len(string.ascii_uppercase)
+LETTERS_COUNT = len(string.ascii_letters) ##LOWERCASE_CHARS + UPPERCASE_CHARS
+DIGITS_COUNT = len(string.digits)
+PUNCTUATION_COUNT = len(string.punctuation)
+CHARACTERS_TO_CONVERT = LETTERS_COUNT + DIGITS_COUNT + PUNCTUATION_COUNT
 
 
 random.seed(SEED)
-dicionary = {}
+dictionary = {}
 code_set = set()
 
 ##FUNCTIONS
 def encode_letter ():
     while True:
-        encoded_letter = ""   # reset en cada intento
+        encoded_letter = ""  # reset en cada intento
         for j in range(SYMBOLS_PER_LETTER):
             encoded_letter = encoded_letter + random.choice(ELIGIBLE_SYMBOLS)
-        if encoded_letter not in code_set:   # ¿qué condición te dice "este código sirve, salí del loop"?
+        if encoded_letter not in code_set:
             break
     return encoded_letter
 
 ## """MAIN"""
 for i in range (CHARACTERS_TO_CONVERT):
-    letter_to_encode = string.ascii_lowercase[i]
+    if (i <= (LETTERS_COUNT -1)):
+        letter_to_encode = string.ascii_letters[i]
+    elif ((i > (LETTERS_COUNT -1)) and (i <= ((LETTERS_COUNT + DIGITS_COUNT) -1))):
+        letter_to_encode = string.digits[i-LETTERS_COUNT ]
+    elif ((i > ((CHARACTERS_TO_CONVERT - PUNCTUATION_COUNT) -1)) and (i <= (CHARACTERS_TO_CONVERT -1))):
+        letter_to_encode = string.punctuation[i-(CHARACTERS_TO_CONVERT - PUNCTUATION_COUNT)]
     encoded_letter = encode_letter()
-    dicionary[letter_to_encode] = encoded_letter
+    dictionary [letter_to_encode] = encoded_letter
     code_set.add(encoded_letter)
-        
-for code in code_set:
-    print (code)
+     
+##for code in code_set:
+##   print (code)
 
-for key, value in dicionary.items():
+for key, value in dictionary .items():
     print(key, value)
